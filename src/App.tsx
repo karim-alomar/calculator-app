b38af0ee9f9ca014f038108ecf833dbed46d7de9
karim_alomar <alomark41@gmail.com>
2022-12-13 00:23:14 +0300

    looping on numbers

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,36 @@
+import { useState } from "react";
 import "./app.css";
-const buttons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const buttons = [
+  ["AC", "!", "%", "/"],
+  [7, 8, 9, "X"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  ["<-", 0, ".", "="],
+];
 function App() {
+  const [result, setResult] = useState(0);
   return (
     <div className="container">
       <div className="calc-container">
         <div className="result">
-          <span>250</span>
+          <span>{result}</span>
         </div>
         <div className="calc-container-buttons">
-          <div className="calc-buttons">
-            <button className="calc-button second-buttons">AC</button>
-            <button className="calc-button second-buttons">8</button>
-            <button className="calc-button second-buttons">%</button>
-            <button className="calc-button main-buttons">/</button>
-          </div>
-          <div className="calc-buttons">
-            <button className="calc-button">7</button>
-            <button className="calc-button">8</button>
-            <button className="calc-button">9</button>
-            <button className="calc-button main-buttons">X</button>
-          </div>
-          <div className="calc-buttons">
-            <button className="calc-button">6</button>
-            <button className="calc-button">5</button>
-            <button className="calc-button">4</button>
-            <button className="calc-button main-buttons">-</button>
-          </div>
-          <div className="calc-buttons">
-            <button className="calc-button">3</button>
-            <button className="calc-button">2</button>
-            <button className="calc-button">1</button>
-            <button className="calc-button main-buttons">+</button>
-          </div>
-          <div className="calc-buttons">
-            <button className="calc-button">
-              <i className="fa-solid fa-rotate-left"></i>
-            </button>
-            <button className="calc-button">0</button>
-            <button className="calc-button">.</button>
-            <button className="calc-button main-buttons">=</button>
-          </div>
+          {buttons.map((collection, collectionIndex) => (
+            <div className="calc-buttons" key={collectionIndex}>
+              {collection.map((btn, buttonIndex) => (
+                <button
+                  value={btn}
+                  className={`calc-button 
+                    ${buttonIndex == 3 ? "main-buttons" : "second-buttons"}
+                  `}
+                  key={buttonIndex}
+                >
+                  {btn}
+                </button>
+              ))}
+            </div>
+          ))}
         </div>
       </div>
     </div>
